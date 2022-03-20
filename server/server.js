92a42b1c7da9aa2919aa4229e19e81493f31fa4a
tepsurkaev <unknownsayid@mail.ru>
2022-03-20 05:09:14 +0300

    create login endpoint

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const fs = require("fs");
 const jsonServer = require("json-server");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 const { body, validationResult } = require("express-validator");
 
 const { users } = JSON.parse(fs.readFileSync("./users.json", "utf-8"));
@@ -72,6 +74,39 @@ server.post(
     });
   }
 );
+
+const createToken = (payload) => {
+  return jwt.sign(payload, process.env.SECRET_KEY, {
+    expiresIn: "1d",
+  });
+};
+
+const isRegistered = (email, password) => {
+  return (
+    users.findIndex(
+      (user) => user.email === email && user.password === password
+    ) !== -1
+  );
+};
+
+server.post("/login", (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const candidate = users.find((user) => user.email === email);
+
+    const userName = candidate?.userName;
+
+    if (!isRegistered(email, password)) {
+      return res.status(401).json("Wrong email or password");
+    }
+
+    const token = createToken({ email, password, userName });
+    return res.status(200).json(token);
+  } catch (e) {
+    return res.json(e);
+  }
+});
 
 server.listen(8000, () => {
   console.log("Server started on port 8000");
